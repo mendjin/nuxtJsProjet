25cbb1a9539be8ff826681edeeeb56cf52dc2617
mendjin michelle <mendjinmichelle@gmail.com>
2023-08-22 12:54:21 +0200

    creation de 4 bloc sur la meme ligne

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
     <div class="container-fluid">
+
+      <div class="col-12 col-md-3 mb-3 mb-sm-0">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Bloc 1</h5>
+                        </div>
+                    </div>
+      </div>
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Bloc 2</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Bloc 3</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Bloc 4</h5>
+                    </div>
+                </div>
+            </div>
     </div>
   </div>
 </template>
