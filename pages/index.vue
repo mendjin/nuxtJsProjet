42edc33eab653276b2b718021599c157e66bb749
mendjin michelle <mendjinmichelle@gmail.com>
2023-08-22 15:11:17 +0200

    ajout du bouton et des images

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
+    <h1 class="text-center">Ma nouvelle page bootstrap</h1>
     <div class="container-fluid">
       <div class="row">
       <div class="col-12 col-md-3 mb-3 mb-sm-0">
                     <div class="card text-bg-primary mb-3">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmaRwV99KvcXv3XSdpLw52XuMll_q8DlzZg&usqp=CAU" 
+                       class="card-img-top" alt="basket1">
                         <div class="card-body">
                             <h5 class="card-title">Bloc 1</h5>
                         </div>
                     </div>
       </div>
-            <div class="col-3">
+            <div class="col-12 col-md-3">
                 <div class="card text-bg-secondary mb-3">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4L2NPpGnm_pP9d8AiJ2meOx2r33aaOQe4w&usqp=CAU" 
+                       class="card-img-top" alt="basket2">
                     <div class="card-body">
                         <h5 class="card-title">Bloc 2</h5>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-12 col-md-3">
                 <div class="card text-bg-success mb-3">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmaRwV99KvcXv3XSdpLw52XuMll_q8DlzZg&usqp=CAU" 
+                       class="card-img-top" alt="basket2">
                     <div class="card-body">
                         <h5 class="card-title">Bloc 3</h5>
                     </div>
                 </div>
             </div>
-            <div class="col-sm-3">
+            <div class="col-12 col-md-3">
                 <div class="card text-bg-danger mb-3">
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4L2NPpGnm_pP9d8AiJ2meOx2r33aaOQe4w&usqp=CAU" 
+                       class="card-img-top" alt="basket2">
                     <div class="card-body">
                         <h5 class="card-title">Bloc 4</h5>
                     </div>
@@ -32,22 +41,28 @@
             </div>
           </div>
             <div class="row">
-                <div class="col-sm-4 mb-3 mb-sm-0">
+                <div class="col-12 col-md-4">
                         <div class="card text-bg-warning mb-3">
+                          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4L2NPpGnm_pP9d8AiJ2meOx2r33aaOQe4w&usqp=CAU" 
+                          class="card-img-top" alt="basket2">
                             <div class="card-body">
                                 <h5 class="card-title">Bloc 1</h5>
                             </div>
                         </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-12 col-md-4">
                     <div class="card text-bg-success mb-3">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmaRwV99KvcXv3XSdpLw52XuMll_q8DlzZg&usqp=CAU" 
+                       class="card-img-top" alt="basket2">
                         <div class="card-body">
                             <h5 class="card-title">Bloc 2</h5>
                         </div>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-12 col-md-4">
                     <div class="card text-bg-info mb-3">
+                      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4L2NPpGnm_pP9d8AiJ2meOx2r33aaOQe4w&usqp=CAU" 
+                       class="card-img-top" alt="basket2">
                         <div class="card-body">
                             <h5 class="card-title">Bloc 3</h5>
                         </div>
@@ -56,15 +71,19 @@
                 </div> 
             
                 <div class="row">
-                    <div class="col-sm-6 mb-3 mb-sm-0">
+                    <div class="col-12 col-md-6">
                             <div class="card text-bg-info mb-3">
+                               <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRo4L2NPpGnm_pP9d8AiJ2meOx2r33aaOQe4w&usqp=CAU" 
+                               class="card-img-top" alt="basket2">
                                 <div class="card-body">
                                     <h5 class="card-title">Bloc 1</h5>
                                 </div>
                             </div>
                     </div>
-                    <div class="col-sm-6">
+                    <div class="col-12 col-md-6">
                         <div class="card text-bg-dark mb-3">
+                           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmaRwV99KvcXv3XSdpLw52XuMll_q8DlzZg&usqp=CAU" 
+                           class="card-img-top" alt="basket2">
                             <div class="card-body">
                                 <h5 class="card-title">Bloc 2</h5>
                             </div>
@@ -72,6 +91,11 @@
                     </div>
                 </div>
     </div>
+    
+   <div>
+    <button type="button" class="btn btn-success">OK</button>
+   </div>
+    
   </div>
 </template>
 
